1dc37868c8c9448c52c40cc947f2422423c99b79
zkjkceo <fyrenx.parent@gmail.com>
2026-03-31 23:09:27 +0200

    Add files via upload

---
+++ EntitySkeleton.java
@@ -0,0 +1,318 @@
+package net.minecraft.src;
+
+import java.util.Calendar;
+
+public class EntitySkeleton extends EntityMob implements IRangedAttackMob {
+	private EntityAIArrowAttack aiArrowAttack = new EntityAIArrowAttack(this, 0.25F, 20, 60, 15.0F);
+	private EntityAIAttackOnCollide aiAttackOnCollide = new EntityAIAttackOnCollide(this, EntityPlayer.class, 0.31F, false);
+
+	public EntitySkeleton(World var1) {
+		super(var1);
+		this.texture = "/mob/skeleton.png";
+		this.moveSpeed = 0.25F;
+		this.tasks.addTask(1, new EntityAISwimming(this));
+		this.tasks.addTask(2, new EntityAIRestrictSun(this));
+		this.tasks.addTask(3, new EntityAIFleeSun(this, this.moveSpeed));
+		this.tasks.addTask(5, new EntityAIWander(this, this.moveSpeed));
+		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(6, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 16.0F, 0, true));
+		if(var1 != null && !var1.isRemote) {
+			this.setCombatTask();
+		}
+
+	}
+
+	protected void entityInit() {
+		super.entityInit();
+		this.dataWatcher.addObject(13, new Byte((byte)0));
+	}
+
+	public boolean isAIEnabled() {
+		return true;
+	}
+
+	public int getMaxHealth() {
+		return 20;
+	}
+
+	protected String getLivingSound() {
+		return "mob.skeleton.say";
+	}
+
+	protected String getHurtSound() {
+		return "mob.skeleton.hurt";
+	}
+
+	protected String getDeathSound() {
+		return "mob.skeleton.death";
+	}
+
+	protected void playStepSound(int var1, int var2, int var3, int var4) {
+		this.playSound("mob.skeleton.step", 0.15F, 1.0F);
+	}
+
+	public boolean attackEntityAsMob(Entity var1) {
+		if(super.attackEntityAsMob(var1)) {
+			if(this.getSkeletonType() == 1 && var1 instanceof EntityLiving) {
+				((EntityLiving)var1).addPotionEffect(new PotionEffect(Potion.wither.id, 200));
+			}
+
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getAttackStrength(Entity var1) {
+		if(this.getSkeletonType() == 1) {
+			ItemStack var2 = this.getHeldItem();
+			int var3 = 4;
+			if(var2 != null) {
+				var3 += var2.getDamageVsEntity(this);
+			}
+
+			return var3;
+		} else {
+			return super.getAttackStrength(var1);
+		}
+	}
+
+	public EnumCreatureAttribute getCreatureAttribute() {
+		return EnumCreatureAttribute.UNDEAD;
+	}
+
+	public void onLivingUpdate() {
+		if(this.worldObj.isDaytime() && !this.worldObj.isRemote) {
+			float var1 = this.getBrightness(1.0F);
+			if(var1 > 0.5F && this.rand.nextFloat() * 30.0F < (var1 - 0.4F) * 2.0F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ))) {
+				boolean var2 = true;
+				ItemStack var3 = this.getCurrentItemOrArmor(4);
+				if(var3 != null) {
+					if(var3.isItemStackDamageable()) {
+						var3.setItemDamage(var3.getItemDamageForDisplay() + this.rand.nextInt(2));
+						if(var3.getItemDamageForDisplay() >= var3.getMaxDamage()) {
+							this.renderBrokenItemStack(var3);
+							this.setCurrentItemOrArmor(4, (ItemStack)null);
+						}
+					}
+
+					var2 = false;
+				}
+
+				if(var2) {
+					this.setFire(8);
+				}
+			}
+		}
+
+		if(this.worldObj.isRemote && this.getSkeletonType() == 1) {
+			this.setSize(0.72F, 2.34F);
+		}
+
+		super.onLivingUpdate();
+	}
+
+	public void onDeath(DamageSource var1) {
+		super.onDeath(var1);
+		if(var1.getSourceOfDamage() instanceof EntityArrow && var1.getEntity() instanceof EntityPlayer) {
+			EntityPlayer var2 = (EntityPlayer)var1.getEntity();
+			double var3 = var2.posX - this.posX;
+			double var5 = var2.posZ - this.posZ;
+			if(var3 * var3 + var5 * var5 >= 2500.0D) {
+				var2.triggerAchievement(AchievementList.snipeSkeleton);
+			}
+		}
+
+	}
+
+	protected int getDropItemId() {
+		return Item.arrow.itemID;
+	}
+
+	protected void dropFewItems(boolean var1, int var2) {
+		int var3;
+		int var4;
+		if(this.getSkeletonType() == 1) {
+			var3 = this.rand.nextInt(3 + var2) - 1;
+
+			for(var4 = 0; var4 < var3; ++var4) {
+				this.dropItem(Item.coal.itemID, 1);
+			}
+		} else {
+			var3 = this.rand.nextInt(3 + var2);
+
+			for(var4 = 0; var4 < var3; ++var4) {
+				this.dropItem(Item.arrow.itemID, 1);
+			}
+		}
+
+		var3 = this.rand.nextInt(3 + var2);
+
+		for(var4 = 0; var4 < var3; ++var4) {
+			this.dropItem(Item.bone.itemID, 1);
+		}
+
+	}
+
+	protected void dropRareDrop(int var1) {
+		if(this.getSkeletonType() == 1) {
+			this.entityDropItem(new ItemStack(Item.skull.itemID, 1, 1), 0.0F);
+		}
+		else if(var1>0) {
+			if(this.rand.nextInt(100) == 0) {
+				ItemStack item = Item.enchantedBook.func_92109_a(rand);
+				this.entityDropItem(item, 1); //enchanted book, roughly 0.005% (1 in 20000)
+			}
+		}
+	}
+
+	protected void addRandomArmor() {
+		if(this.rand.nextFloat() < 0.07F) {
+			int var1 = this.rand.nextInt(2);
+			float var2 = 0.1F;
+			if(this.rand.nextFloat() < 0.095F) {
+				++var1;
+			}
+
+			if(this.rand.nextFloat() < 0.095F) {
+				++var1;
+			}
+
+			if(this.rand.nextFloat() < 0.095F) {
+				++var1;
+			}
+
+			for(int var3 = 3; var3 >= 0; --var3) {
+				ItemStack var4 = this.getCurrentArmor(var3);
+				if(var3 < 3 && this.rand.nextFloat() < var2) {
+					break;
+				}
+
+				if(var4 == null) {
+					Item var5 = getArmorItemForSlot(var3 + 1, var1);
+					if(var5 != null) {
+						this.setCurrentItemOrArmor(var3 + 1, new ItemStack(var5));
+					}
+				}
+			}
+		}
+		this.setCurrentItemOrArmor(0, new ItemStack(Item.bow));
+		//Regardless of difficulty skeletons spawn with armor as if it was hard difficulty.
+	}
+	
+	protected void func_82162_bC() {
+		if(this.getHeldItem() != null && this.rand.nextFloat() < 0.2F) {
+			double r = this.rand.nextDouble();
+			int level = (int)(5 + Math.pow(r, 4) * 40); //new curve for enchanted equipment, allows for op items but mostly rolls garbage
+			EnchantmentHelper.addRandomEnchantment(this.rand, this.getHeldItem(), level);
+		} 
+		for(int var1 = 0; var1 < 4; ++var1) {
+			double r = this.rand.nextDouble();
+			int level = (int)(5 + Math.pow(r, 4) * 40); //new curve for enchanted equipment, allows for op items but mostly rolls garbage
+			ItemStack var2 = this.getCurrentArmor(var1);
+			if(var2 != null && this.rand.nextFloat() < 0.2F) {
+				EnchantmentHelper.addRandomEnchantment(this.rand, var2, level);
+			}
+		}
+		//Regardless of difficulty their equipment has the same chance to be enchanted as if it was hard difficulty
+	}
+
+	public String getTexture() {
+		return this.getSkeletonType() == 1 ? "/mob/skeleton_wither.png" : super.getTexture();
+	}
+
+	public void initCreature() {
+		if(this.worldObj.provider instanceof WorldProviderHell && this.getRNG().nextInt(5) > 0) {
+			this.tasks.addTask(4, this.aiAttackOnCollide);
+			this.setSkeletonType(1);
+			this.setCurrentItemOrArmor(0, new ItemStack(Item.swordStone));
+		} else {
+			this.tasks.addTask(4, this.aiArrowAttack);
+			this.addRandomArmor();
+			this.func_82162_bC();
+		}
+
+		this.setCanPickUpLoot(this.rand.nextFloat() < 0.45F);
+		if(this.getCurrentItemOrArmor(4) == null) {
+			Calendar var1 = this.worldObj.getCurrentDate();
+			if(var1.get(2) + 1 == 10 && var1.get(5) == 31 && this.rand.nextFloat() < 0.25F) {
+				this.setCurrentItemOrArmor(4, new ItemStack(this.rand.nextFloat() < 0.1F ? Block.pumpkinLantern : Block.pumpkin));
+				this.equipmentDropChances[4] = 0.0F;
+			}
+		}
+
+	}
+
+	public void setCombatTask() {
+		this.tasks.removeTask(this.aiAttackOnCollide);
+		this.tasks.removeTask(this.aiArrowAttack);
+		ItemStack var1 = this.getHeldItem();
+		if(var1 != null && var1.itemID == Item.bow.itemID) {
+			this.tasks.addTask(4, this.aiArrowAttack);
+		} else {
+			this.tasks.addTask(4, this.aiAttackOnCollide);
+		}
+
+	}
+
+	public void attackEntityWithRangedAttack(EntityLiving var1, float var2) {
+		EntityArrow var3 = new EntityArrow(this.worldObj, this, var1, 1.6F, (float)(14 - this.worldObj.difficultySetting * 4));
+		int var4 = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, this.getHeldItem());
+		int var5 = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, this.getHeldItem());
+		var3.setDamage((double)(var2 * 2.0F) + this.rand.nextGaussian() * 0.25D + (double)((float)this.worldObj.difficultySetting * 0.11F));
+		if(var4 > 0) {
+			var3.setDamage(var3.getDamage() + (double)var4 * 0.5D + 0.5D);
+		}
+
+		if(var5 > 0) {
+			var3.setKnockbackStrength(var5);
+		}
+
+		if(EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, this.getHeldItem()) > 0 || this.getSkeletonType() == 1) {
+			var3.setFire(100);
+		}
+
+		this.playSound("random.bow", 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
+		this.worldObj.spawnEntityInWorld(var3);
+	}
+
+	public int getSkeletonType() {
+		return this.dataWatcher.getWatchableObjectByte(13);
+	}
+
+	public void setSkeletonType(int var1) {
+		this.dataWatcher.updateObject(13, Byte.valueOf((byte)var1));
+		this.isImmuneToFire = var1 == 1;
+		if(var1 == 1) {
+			this.setSize(0.72F, 2.34F);
+		} else {
+			this.setSize(0.6F, 1.8F);
+		}
+
+	}
+
+	public void readEntityFromNBT(NBTTagCompound var1) {
+		super.readEntityFromNBT(var1);
+		if(var1.hasKey("SkeletonType")) {
+			byte var2 = var1.getByte("SkeletonType");
+			this.setSkeletonType(var2);
+		}
+
+		this.setCombatTask();
+	}
+
+	public void writeEntityToNBT(NBTTagCompound var1) {
+		super.writeEntityToNBT(var1);
+		var1.setByte("SkeletonType", (byte)this.getSkeletonType());
+	}
+
+	public void setCurrentItemOrArmor(int var1, ItemStack var2) {
+		super.setCurrentItemOrArmor(var1, var2);
+		if(!this.worldObj.isRemote && var1 == 0) {
+			this.setCombatTask();
+		}
+
+	}
+}
---
+++ EntityZombie.java
@@ -0,0 +1,394 @@
+package net.minecraft.src;
+
+import java.util.Calendar;
+
+public class EntityZombie extends EntityMob {
+	private int conversionTime = 0;
+
+	public EntityZombie(World var1) {
+		super(var1);
+		this.texture = "/mob/zombie.png";
+		this.moveSpeed = 0.23F;
+		this.getNavigator().setBreakDoors(true);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIBreakDoor(this));
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, this.moveSpeed, false));
+		this.tasks.addTask(3, new EntityAIAttackOnCollide(this, EntityVillager.class, this.moveSpeed, true));
+		this.tasks.addTask(4, new EntityAIMoveTwardsRestriction(this, this.moveSpeed));
+		this.tasks.addTask(5, new EntityAIMoveThroughVillage(this, this.moveSpeed, false));
+		this.tasks.addTask(6, new EntityAIWander(this, this.moveSpeed));
+		this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(7, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 16.0F, 0, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 16.0F, 0, false));
+	}
+
+	protected int func_96121_ay() {
+		return 40;
+	}
+
+	public float getSpeedModifier() {
+		return super.getSpeedModifier() * (this.isChild() ? 1.5F : 1.0F);
+	}
+
+	protected void entityInit() {
+		super.entityInit();
+		this.getDataWatcher().addObject(12, Byte.valueOf((byte)0));
+		this.getDataWatcher().addObject(13, Byte.valueOf((byte)0));
+		this.getDataWatcher().addObject(14, Byte.valueOf((byte)0));
+	}
+
+	public String getTexture() {
+		return this.isVillager() ? "/mob/zombie_villager.png" : "/mob/zombie.png";
+	}
+
+	public int getMaxHealth() {
+		return 20;
+	}
+
+	public int getTotalArmorValue() {
+		int var1 = super.getTotalArmorValue() + 2;
+		if(var1 > 20) {
+			var1 = 20;
+		}
+
+		return var1;
+	}
+
+	protected boolean isAIEnabled() {
+		return true;
+	}
+
+	public boolean isChild() {
+		return this.getDataWatcher().getWatchableObjectByte(12) == 1;
+	}
+
+	public void setChild(boolean var1) {
+		this.getDataWatcher().updateObject(12, Byte.valueOf((byte)1));
+	}
+
+	public boolean isVillager() {
+		return this.getDataWatcher().getWatchableObjectByte(13) == 1;
+	}
+
+	public void setVillager(boolean var1) {
+		this.getDataWatcher().updateObject(13, Byte.valueOf((byte)(var1 ? 1 : 0)));
+	}
+
+	public void onLivingUpdate() {
+		if(this.worldObj.isDaytime() && !this.worldObj.isRemote && !this.isChild()) {
+			float var1 = this.getBrightness(1.0F);
+			if(var1 > 0.5F && this.rand.nextFloat() * 30.0F < (var1 - 0.4F) * 2.0F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ))) {
+				boolean var2 = true;
+				ItemStack var3 = this.getCurrentItemOrArmor(4);
+				if(var3 != null) {
+					if(var3.isItemStackDamageable()) {
+						var3.setItemDamage(var3.getItemDamageForDisplay() + this.rand.nextInt(2));
+						if(var3.getItemDamageForDisplay() >= var3.getMaxDamage()) {
+							this.renderBrokenItemStack(var3);
+							this.setCurrentItemOrArmor(4, (ItemStack)null);
+						}
+					}
+
+					var2 = false;
+				}
+
+				if(var2) {
+					this.setFire(8);
+				}
+			}
+		}
+
+		super.onLivingUpdate();
+	}
+
+	public void onUpdate() {
+		if(!this.worldObj.isRemote && this.isConverting()) {
+			int var1 = this.getConversionTimeBoost();
+			this.conversionTime -= var1;
+			if(this.conversionTime <= 0) {
+				this.convertToVillager();
+			}
+		}
+
+		super.onUpdate();
+	}
+
+	public boolean attackEntityAsMob(Entity var1) {
+		boolean var2 = super.attackEntityAsMob(var1);
+		if(var2 && this.getHeldItem() == null && this.isBurning() && this.rand.nextFloat() < (float)this.worldObj.difficultySetting * 0.3F) {
+			var1.setFire(2 * this.worldObj.difficultySetting);
+		}
+
+		return var2;
+	}
+
+	public int getAttackStrength(Entity var1) {
+		ItemStack var2 = this.getHeldItem();
+		float var3 = (float)(this.getMaxHealth() - this.getHealth()) / (float)this.getMaxHealth();
+		int var4 = 3 + MathHelper.floor_float(var3 * 4.0F);
+		if(var2 != null) {
+			var4 += var2.getDamageVsEntity(this);
+		}
+
+		return var4;
+	}
+
+	protected String getLivingSound() {
+		return "mob.zombie.say";
+	}
+
+	protected String getHurtSound() {
+		return "mob.zombie.hurt";
+	}
+
+	protected String getDeathSound() {
+		return "mob.zombie.death";
+	}
+
+	protected void playStepSound(int var1, int var2, int var3, int var4) {
+		this.playSound("mob.zombie.step", 0.15F, 1.0F);
+	}
+
+	protected int getDropItemId() {
+		return Item.rottenFlesh.itemID;
+	}
+
+	public EnumCreatureAttribute getCreatureAttribute() {
+		return EnumCreatureAttribute.UNDEAD;
+	}
+
+	protected void dropRareDrop(int var1) {
+		if(var1>0) {
+			if(this.rand.nextInt(5) == 0) {
+				ItemStack item = Item.enchantedBook.func_92109_a(rand);
+				this.entityDropItem(item, 1); //enchanted book, roughly 0.1% (1 in 1000)
+			} else {
+				this.dropItem(Item.beetroot.itemID, 1); //beetroot, roughly 0.4% (1 in 250)
+			}
+		} else {
+			switch(this.rand.nextInt(3)) {
+			case 0:
+				this.dropItem(Item.ingotIron.itemID, 1);
+				break;
+			case 1:
+				this.dropItem(Item.carrot.itemID, 1);
+				break;
+			case 2:
+				this.dropItem(Item.potato.itemID, 1);
+			}
+		}
+	}
+
+	protected void addRandomArmor() {
+		if(this.rand.nextFloat() < 0.07F) {
+			int var1 = this.rand.nextInt(2);
+			float var2 = 0.1F;
+			if(this.rand.nextFloat() < 0.095F) {
+				++var1;
+			}
+
+			if(this.rand.nextFloat() < 0.095F) {
+				++var1;
+			}
+
+			if(this.rand.nextFloat() < 0.095F) {
+				++var1;
+			}
+
+			for(int var3 = 3; var3 >= 0; --var3) {
+				ItemStack var4 = this.getCurrentArmor(var3);
+				if(var3 < 3 && this.rand.nextFloat() < var2) {
+					break;
+				}
+
+				if(var4 == null) {
+					Item var5 = getArmorItemForSlot(var3 + 1, var1);
+					if(var5 != null) {
+						this.setCurrentItemOrArmor(var3 + 1, new ItemStack(var5));
+					}
+				}
+			}
+		}
+		if(this.rand.nextFloat() < 0.05F) {
+			int var1 = this.rand.nextInt(3);
+			if(var1 == 0) {
+				this.setCurrentItemOrArmor(0, new ItemStack(Item.swordIron));
+			} else {
+				this.setCurrentItemOrArmor(0, new ItemStack(Item.shovelIron));
+			}
+		}
+		//Regardless of difficulty zombies spawn with armor as if it was hard difficulty.
+	}
+	
+	protected void func_82162_bC() {
+		if(this.getHeldItem() != null && this.rand.nextFloat() < 0.2F) {
+			double r = this.rand.nextDouble();
+			int level = (int)(5 + Math.pow(r, 4) * 40); //new curve for enchanted equipment, allows for op items but mostly rolls garbage
+			EnchantmentHelper.addRandomEnchantment(this.rand, this.getHeldItem(), level);
+		} 
+		for(int var1 = 0; var1 < 4; ++var1) {
+			double r = this.rand.nextDouble();
+			int level = (int)(5 + Math.pow(r, 4) * 40); //new curve for enchanted equipment, allows for op items but mostly rolls garbage
+			ItemStack var2 = this.getCurrentArmor(var1);
+			if(var2 != null && this.rand.nextFloat() < 0.2F) {
+				EnchantmentHelper.addRandomEnchantment(this.rand, var2, level);
+			}
+		}
+		//Regardless of difficulty their equipment has the same chance to be enchanted as if it was hard difficulty
+	}
+
+	public void writeEntityToNBT(NBTTagCompound var1) {
+		super.writeEntityToNBT(var1);
+		if(this.isChild()) {
+			var1.setBoolean("IsBaby", true);
+		}
+
+		if(this.isVillager()) {
+			var1.setBoolean("IsVillager", true);
+		}
+
+		var1.setInteger("ConversionTime", this.isConverting() ? this.conversionTime : -1);
+	}
+
+	public void readEntityFromNBT(NBTTagCompound var1) {
+		super.readEntityFromNBT(var1);
+		if(var1.getBoolean("IsBaby")) {
+			this.setChild(true);
+		}
+
+		if(var1.getBoolean("IsVillager")) {
+			this.setVillager(true);
+		}
+
+		if(var1.hasKey("ConversionTime") && var1.getInteger("ConversionTime") > -1) {
+			this.startConversion(var1.getInteger("ConversionTime"));
+		}
+
+	}
+
+	public void onKillEntity(EntityLiving var1) {
+		super.onKillEntity(var1);
+		if(this.worldObj.difficultySetting >= 2 && var1 instanceof EntityVillager) {
+			if(this.worldObj.difficultySetting == 2 && this.rand.nextBoolean()) {
+				return;
+			}
+
+			EntityZombie var2 = new EntityZombie(this.worldObj);
+			var2.func_82149_j(var1);
+			this.worldObj.removeEntity(var1);
+			var2.initCreature();
+			var2.setVillager(true);
+			if(var1.isChild()) {
+				var2.setChild(true);
+			}
+
+			this.worldObj.spawnEntityInWorld(var2);
+			this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1016, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+		}
+
+	}
+
+	public void initCreature() {
+		this.setCanPickUpLoot(this.rand.nextFloat() < 0.45F);
+		if(this.worldObj.rand.nextFloat() < 0.05F) {
+			this.setVillager(true);
+		}
+
+		this.addRandomArmor();
+		this.func_82162_bC();
+		if(this.getCurrentItemOrArmor(4) == null) {
+			Calendar var1 = this.worldObj.getCurrentDate();
+			if(var1.get(2) + 1 == 10 && var1.get(5) == 31 && this.rand.nextFloat() < 0.25F) {
+				this.setCurrentItemOrArmor(4, new ItemStack(this.rand.nextFloat() < 0.1F ? Block.pumpkinLantern : Block.pumpkin));
+				this.equipmentDropChances[4] = 0.0F;
+			}
+		}
+		if (this.rand.nextFloat() < 0.05F) {
+			this.setChild(true);
+		}
+
+	}
+
+	public boolean interact(EntityPlayer var1) {
+		ItemStack var2 = var1.getCurrentEquippedItem();
+		if(var2 != null && var2.getItem() == Item.appleGold && var2.getItemDamage() == 0 && this.isVillager() && this.isPotionActive(Potion.weakness)) {
+			if(!var1.capabilities.isCreativeMode) {
+				--var2.stackSize;
+			}
+
+			if(var2.stackSize <= 0) {
+				var1.inventory.setInventorySlotContents(var1.inventory.currentItem, (ItemStack)null);
+			}
+
+			if(!this.worldObj.isRemote) {
+				this.startConversion(this.rand.nextInt(2401) + 3600);
+			}
+
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	protected void startConversion(int var1) {
+		this.conversionTime = var1;
+		this.getDataWatcher().updateObject(14, Byte.valueOf((byte)1));
+		this.removePotionEffect(Potion.weakness.id);
+		this.addPotionEffect(new PotionEffect(Potion.damageBoost.id, var1, Math.min(this.worldObj.difficultySetting - 1, 0)));
+		this.worldObj.setEntityState(this, (byte)16);
+	}
+
+	public void handleHealthUpdate(byte var1) {
+		if(var1 == 16) {
+			this.worldObj.playSound(this.posX + 0.5D, this.posY + 0.5D, this.posZ + 0.5D, "mob.zombie.remedy", 1.0F + this.rand.nextFloat(), this.rand.nextFloat() * 0.7F + 0.3F, false);
+		} else {
+			super.handleHealthUpdate(var1);
+		}
+
+	}
+
+	public boolean isConverting() {
+		return this.getDataWatcher().getWatchableObjectByte(14) == 1;
+	}
+
+	protected void convertToVillager() {
+		EntityVillager var1 = new EntityVillager(this.worldObj);
+		var1.func_82149_j(this);
+		var1.initCreature();
+		var1.func_82187_q();
+		if(this.isChild()) {
+			var1.setGrowingAge(-24000);
+		}
+
+		this.worldObj.removeEntity(this);
+		this.worldObj.spawnEntityInWorld(var1);
+		var1.addPotionEffect(new PotionEffect(Potion.confusion.id, 200, 0));
+		this.worldObj.playAuxSFXAtEntity((EntityPlayer)null, 1017, (int)this.posX, (int)this.posY, (int)this.posZ, 0);
+	}
+
+	protected int getConversionTimeBoost() {
+		int var1 = 1;
+		if(this.rand.nextFloat() < 0.01F) {
+			int var2 = 0;
+
+			for(int var3 = (int)this.posX - 4; var3 < (int)this.posX + 4 && var2 < 14; ++var3) {
+				for(int var4 = (int)this.posY - 4; var4 < (int)this.posY + 4 && var2 < 14; ++var4) {
+					for(int var5 = (int)this.posZ - 4; var5 < (int)this.posZ + 4 && var2 < 14; ++var5) {
+						int var6 = this.worldObj.getBlockId(var3, var4, var5);
+						if(var6 == Block.fenceIron.blockID || var6 == Block.bed.blockID) {
+							if(this.rand.nextFloat() < 0.3F) {
+								++var1;
+							}
+
+							++var2;
+						}
+					}
+				}
+			}
+		}
+
+		return var1;
+	}
+}
