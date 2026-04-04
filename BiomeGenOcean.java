7a034f23571c58ac784c9b826a6bcfa01b17828f
zkjkceo <fyrenx.parent@gmail.com>
2026-04-04 22:10:27 +0200

    Delete BiomeGenOcean.java

--- BiomeGenOcean.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public class BiomeGenOcean extends BiomeGenBase {
-	public BiomeGenOcean(int var1) {
-		super(var1);
-		this.theBiomeDecorator.treesPerChunk = 3; //some trees
-		this.spawnableCreatureList.clear();
-	}
-}
