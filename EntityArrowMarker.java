8634620adf358f47f4619af6546996df62725853
zkjkceo <fyrenx.parent@gmail.com>
2026-04-08 16:51:02 +0200

    Add files via upload

--- EntityArrowMarker.java
+++ EntityArrowMarker.java
@@ -3,15 +3,20 @@ package net.minecraft.src;
 import java.util.List;
 
 public class EntityArrowMarker extends EntityArrow {
+	
 	public EntityArrowMarker(World var1) {
 		super(var1);
 	}
 
 	public void onUpdate() {
 		if (!this.worldObj.isRemote) {
-        this.motionX = 0;
-        this.motionY = 0;
-        this.motionZ = 0;
+			if (this.ticksExisted > 5 && this.riddenByEntity == null) {
+				this.setDead();
+				return;
+			}
+			this.motionX = 0;
+			this.motionY = 0;
+			this.motionZ = 0;
 		} else {
 			this.motionX = 0;
 			this.motionY = 0;
